058de93c15264c3dda958fef4e50eb7ef1a83fcd
Uzi89101 <154096847+Uzi89101@users.noreply.github.com>
2024-01-02 17:05:19 +0100

    Add files via upload

---
+++ todo-app/src/components/TodoForm.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+import { useTodoListStore } from '../stores/todoList';
+
+const todo = ref('')
+
+const store = useTodoListStore()
+
+function addItemandClear(item){
+    if(item.length===0){
+        return
+    }
+    store.addTodo(item)
+    todo.value=''
+}
+
+</script>
+<template>
+    <div>
+        <form @submit.prevent="addItemandClear(todo)">
+            <input v-model="todo" type="text">
+            <button>Add</button>
+        </form>
+    </div>
+</template>
+<style scoped>
+
+</style>
---
+++ todo-app/src/stores/todoList.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+
+export const useTodoListStore = defineStore("todoList", {
+  state:()=>({
+    todoList:[]
+  }),
+  actions:{
+    addTodo(item){
+        this.todoList.push({item,id:this.id++,completed:false})
+    },
+    deleteTodo(itemId){
+        this.todoList = this.todoList.filter((Object)=>{
+            return Object.id !== itemId
+        })
+
+    }
+  }
+})
